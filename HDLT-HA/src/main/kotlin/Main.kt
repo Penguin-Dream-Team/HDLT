ade35522a2603210346ed3b1f6081b7fcf990937
João Soares <jsoares.public@gmail.com>
2021-04-12 11:10:15 +0100

    Added json stuff

--- HDLT-HA/src/main/kotlin/Main.kt
+++ HDLT-HA/src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package sec.hdlt.ha
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+
+@Serializable
+data class Test(val hello: String, val aa: Int)
+
 fun main() {
-    println("Hello there")
+    println(Json { prettyPrint = true }.encodeToString(Test("Helldsods", 213)))
 }
