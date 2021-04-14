08d03a053a43809fb44b5638588c1ae40ff42d02
João Soares <jsoares.public@gmail.com>
2021-04-14 15:50:37 +0100

    Init report database access object

--- HDLT-LocationServer/src/main/kotlin/sec/hdlt/server/dao/ReportDAO.kt
+++ HDLT-LocationServer/src/main/kotlin/sec/hdlt/server/dao/ReportDAO.kt
@@ -1,8 +1,10 @@
 package sec.hdlt.server.dao
 
-import org.jooq.Configuration
-import org.jooq.DSLContext
+import org.jooq.*
 import org.jooq.impl.DSL
+import org.jooq.types.UInteger
+import sec.hdlt.server.data.Coordinates
+import sec.hdlt.server.data.ReportInfo
 import sec.hdlt.server.db.database.Tables.REPORTS
 
 class ReportDAO(
@@ -10,9 +12,37 @@ class ReportDAO(
 ) {
     private val dslContext = DSL.using(conf)
 
-    fun hello(create: DSLContext = dslContext) {
-        val count = create.fetchCount(REPORTS)
-        println("ok: $count")
+    fun hasUserReport(userId: Int, epoch: Int, create: DSLContext = dslContext): Boolean {
+        return create.fetchCount(
+            create.select(REPORTS.ID)
+                .from(REPORTS)
+                .where(REPORTS.USER_ID.eq(userId))
+                .and(REPORTS.EPOCH.eq(epoch))
+        ) != 0
     }
 
+    fun getUserReport(userId: Int, epoch: Int, create: DSLContext = dslContext): ReportInfo {
+        return create.select()
+            .from(REPORTS)
+            .where(REPORTS.USER_ID.eq(userId))
+            .and(REPORTS.EPOCH.eq(epoch))
+            .fetchOne()?.map {
+                ReportInfo(
+                    id = it[REPORTS.USER_ID].toInt(),
+                    epoch = it[REPORTS.EPOCH].toInt(),
+                    coordinates = Coordinates(
+                        x = it[REPORTS.X].toInt(),
+                        y = it[REPORTS.Y].toInt()
+                    )
+                )
+            } ?: throw NotImplementedError("TODO: MAKE A NOT FOUND ERROR WE CAN CATCH OUTSIDE!")
+    }
+
+}
+
+/**
+ * Helper extension to avoid repetition
+ */
+private fun <R : Record> TableField<R, UInteger>.eq(other: Int): Condition {
+    return eq(UInteger.valueOf(other))
 }
