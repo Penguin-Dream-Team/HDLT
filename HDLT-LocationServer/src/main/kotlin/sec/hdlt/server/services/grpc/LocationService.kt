4a1fd68f0f958082ffcdcbd026dee785ed19568a
DMaxter <daniel_matos@outlook.pt>
2021-05-19 14:29:06 +0100

    Fix broadcast

--- HDLT-LocationServer/src/main/kotlin/sec/hdlt/server/services/grpc/LocationService.kt
+++ HDLT-LocationServer/src/main/kotlin/sec/hdlt/server/services/grpc/LocationService.kt
@@ -25,68 +25,74 @@ import javax.crypto.SecretKey
 
 class LocationService : LocationGrpcKt.LocationCoroutineImplBase() {
     override suspend fun submitLocationReport(request: Report.ReportRequest): Report.ReportResponse {
-        val symKey: SecretKey = asymmetricDecipher(Database.key, request.key)
-        val report: LocationReport = requestToLocationReport(symKey, request.nonce, request.ciphertext)
+        try {
+            val symKey: SecretKey = asymmetricDecipher(Database.key, request.key)
+            val report: LocationReport = requestToLocationReport(symKey, request.nonce, request.ciphertext)
 
-        val user = report.id
-        val epoch = report.epoch
-        val coordinates = report.location
-        val sig = report.signature
-        var proofs = report.proofs
+            val user = report.id
+            val epoch = report.epoch
+            val coordinates = report.location
+            val sig = report.signature
+            var proofs = report.proofs
 
-        val decipheredNonce = Base64.getDecoder().decode(request.nonce)
-        val validNonce = try {
-            Database.nonceDAO.storeUserNonce(decipheredNonce, user)
-        } catch (e: DataAccessException) {
-            false
-        }
+            val decipheredNonce = Base64.getDecoder().decode(request.nonce)
+            val validNonce = try {
+                Database.nonceDAO.storeUserNonce(decipheredNonce, user)
+            } catch (e: DataAccessException) {
+                false
+            }
 
-        if (validNonce && RequestValidationService.validateSignature(
-                user,
-                epoch,
-                coordinates,
-                sig
-            ) && !LocationReportService.hasReport(user, epoch)
-        ) {
-            proofs = RequestValidationService.getValidProofs(user, epoch, proofs)
+            if (validNonce && RequestValidationService.validateSignature(
+                    user,
+                    epoch,
+                    coordinates,
+                    sig
+                ) && !LocationReportService.hasReport(user, epoch)
+            ) {
+                proofs = RequestValidationService.getValidProofs(user, epoch, proofs)
 
-            if (proofs.isNotEmpty()) {
-                println("[EPOCH ${report.epoch}] received a write request from user $user")
+                if (proofs.isNotEmpty()) {
+                    println("[EPOCH ${report.epoch}] received a write request from user $user")
 
-                val ack: Boolean
+                    val ack: Boolean
 
-                // Check if writer is byzantine with broadcast
-                if (broadcast(report)) {
-                    GET_REPORT_LISTENERS_LOCK.withLock {
-                        ack = LocationReportService.storeLocationReport(report, epoch, user, coordinates, proofs)
+                    // Check if writer is byzantine with broadcast
+                    if (broadcast(report)) {
+                        GET_REPORT_LISTENERS_LOCK.withLock {
+                            ack = LocationReportService.storeLocationReport(report, epoch, user, coordinates, proofs)
 
-                        // Check if valid report and if there are listeners
-                        if (ack && GET_REPORT_LISTENERS.containsKey(epoch)) {
-                            val epochListeners = GET_REPORT_LISTENERS[epoch]!!
-                            if (epochListeners.containsKey(user)) {
-                                epochListeners[user]!!.stream().forEach {
-                                    it.offer(Unit)
+                            // Check if valid report and if there are listeners
+                            if (ack && GET_REPORT_LISTENERS.containsKey(epoch)) {
+                                val epochListeners = GET_REPORT_LISTENERS[epoch]!!
+                                if (epochListeners.containsKey(user)) {
+                                    epochListeners[user]!!.stream().forEach {
+                                        it.offer(Unit)
+                                    }
+
+                                    epochListeners.remove(user)
                                 }
-
-                                epochListeners.remove(user)
                             }
                         }
+
+                        return Report.ReportResponse.newBuilder().apply {
+                            val messageNonce = generateNonce()
+                            nonce = Base64.getEncoder().encodeToString(messageNonce)
+
+                            ciphertext = symmetricCipher(
+                                symKey,
+                                Json.encodeToString(ack),
+                                messageNonce
+                            )
+                        }.build()
                     }
-
-                    return Report.ReportResponse.newBuilder().apply {
-                        val messageNonce = generateNonce()
-                        nonce = Base64.getEncoder().encodeToString(messageNonce)
-
-                        ciphertext = symmetricCipher(
-                            symKey,
-                            Json.encodeToString(ack),
-                            messageNonce
-                        )
-                    }.build()
                 }
             }
-        }
 
+        } catch (e: Exception) {
+            // TODO: DEBUG
+            println("SUBMIT ERROR")
+            e.printStackTrace()
+        }
         throw Status.INVALID_ARGUMENT.asException()
     }
 
